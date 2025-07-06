4708a5c2ad8a185207c3bdecdecbef8ad91ae285
androkingdom <androkingdom1@gmail.com>
2025-07-06 11:39:28 +0530

    Add netlify-cli dependency and integrate netlify plugin in Vite config
    - Added "netlify-cli" to package.json dependencies.
    - Imported and configured netlify plugin in vite.config.ts for enhanced deployment support.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import devtoolsJson from "vite-plugin-devtools-json";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
   server: {
     port: 5000,
   },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    devtoolsJson(),
+    netlifyPlugin(),
+  ],
 });
